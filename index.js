4a3f1f7de88667070d26d4a388fb1ce210b2d344
jabir-st-dev <127714535+jabir-st-dev@users.noreply.github.com>
2023-03-20 09:31:56 +0600

    multer implemented
    multer is a file library to upload files

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const multer = require("multer");
+
 require('dotenv').config()
 
-var app = express();
+const app = express();
+
+const upload = multer({ dest: "public/files" });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+);
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+    // Stuff to be added later
+    console.log(req.file);
+    res.json({
+        name: req.file.filename,
+        type: req.file.mimetype,
+        size: req.file.size
+    })
+});
 
 
 
